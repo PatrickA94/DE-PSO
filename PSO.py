14975ddcdb6f6c163eba13b6873544d656865a62
TSomann <taylorsomann@gmail.com>
2017-11-29 18:39:01 -0500

    Update PSO.py

--- PSO.py
+++ PSO.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import math
-#from Functions import *
+from Functions import *
 dimensions = 2
 size = 100
 max_nfc = 5000*dimensions
@@ -30,34 +29,35 @@ class Particle(object):
             
     def updatePosition(self):
         for i in range(dimensions):
-            self.position[i] = self.position[i] + self.velocity[i]            
-
-def Ackley(dim):
-    firstSum = 0.0
-    secondSum = 0.0
-    for c in dim:
-        firstSum += c ** 2.0
-        secondSum += np.cos(2.0 * math.pi * c)
-    n = float(len(dim))
-    return -20.0 * math.exp(-0.2 * math.sqrt(firstSum / n)) - math.exp(secondSum / n) + 20 + math.e
+            self.position[i] = self.position[i] + self.velocity[i]
+            if (self.position[i] > 10):
+                self.position[i] = 10
+                self.velocity[i] = -(self.velocity[i]) 
+            elif (self.position[i] < -10):
+                self.position[i] = -10
+                self.velocity[i] = -(self.velocity[i])
 
 def func(points):
-    y = Ackley(points)
+    y = weirerstrass(points)
     return y
 
 def main():
     solution = []
     swarm = []
     
-    w = 0.7
-    w_mod = (0.4)/max_nfc
+    w = 0.9
+    w_mod = (0.5)/max_nfc
     
+    print("Initializing "+str(size)+" particles...")
     for i in range(0,size):
         particle = Particle(dimensions)
         swarm.append(particle)
 
+    print("Iterating "+str(max_nfc)+" times...")
     gbest = swarm[0].position        
     for i in range(max_nfc):
+        if (i%100 == 0):
+            print("Iterating: "+str(i)+"/"+str(max_nfc))
         for s in swarm:
             pbest = s.pbest
             if func(pbest) < func(gbest):
@@ -73,8 +73,22 @@ def main():
             if func(position) < func(pbest):
                 swarm[l].pbest = swarm[l].position
         w = w - w_mod
-    print(solution)
-    print(func(solution))
+    print("X1: "+str(solution[0]))
+    print("X2: "+str(solution[1]))
+    if (dimensions == 5):
+        print("X3: "+str(solution[2]))
+        print("X4: "+str(solution[3]))
+        print("X5: "+str(solution[4]))
+    if (dimensions == 10):
+        print("X3: "+str(solution[2]))
+        print("X4: "+str(solution[3]))
+        print("X5: "+str(solution[4]))
+        print("X6: "+str(solution[5]))
+        print("X7: "+str(solution[6]))
+        print("X8: "+str(solution[7]))
+        print("X9: "+str(solution[8]))
+        print("X10: "+str(solution[9]))
+    print("Result: "+str(func(solution)))
     return solution
     
 main()
