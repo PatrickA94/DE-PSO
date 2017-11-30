d9c94f4a5557dbd548d63bead6025356b3bb1223
AlexMotyka <alex.motyka@uoit.net>
2017-11-30 00:16:13 -0500

    KURWA IT WORKS

--- PSO2.py
+++ PSO2.py
@@ -4,7 +4,7 @@ from Functions import *
 
 dimensions = 2
 size = 100
-max_nfc = 5000 * dimensions
+max_nfc = 3000 * dimensions
 c1 = 2.05
 c2 = 2.05
 
@@ -79,8 +79,10 @@ def main():
             pointlist = []
             if (i % 100 == 0 and i > 0):
                 print("Iterating: " + str(i) + "/" + str(max_nfc))
+                '''
                 print("X1: " + str(solution[0]))
                 print("X2: " + str(solution[1]))
+                '''
                 if (dimensions == 5):
                     print("X3: " + str(solution[2]))
                     print("X4: " + str(solution[3]))
@@ -117,6 +119,11 @@ def main():
                 rundatamax.append(max(func(pointlist)))
                 rundatamean.append(np.mean(func(pointlist)))
                 rundatastd.append(np.std(func(pointlist)))
+        print rundata
+        print rundatamax
+        print rundatamean
+        print rundatastd
+        print "rundata above"
         run = run + 1
         if run%10==0:
             print('On run: ',run)
@@ -131,23 +138,31 @@ def main():
     final_runmean = allrunsmean[:, -1]
     final_runstd = allrunsstd[:, -1]
     plot_runs = np.average(allruns, axis=0)  # This is for the plots
-    print("X1: " + str(solution[0]))
-    print("X2: " + str(solution[1]))
-    if (dimensions == 5):
-        print("X3: " + str(solution[2]))
-        print("X4: " + str(solution[3]))
-        print("X5: " + str(solution[4]))
-    if (dimensions == 10):
-        print("X3: " + str(solution[2]))
-        print("X4: " + str(solution[3]))
-        print("X5: " + str(solution[4]))
-        print("X6: " + str(solution[5]))
-        print("X7: " + str(solution[6]))
-        print("X8: " + str(solution[7]))
-        print("X9: " + str(solution[8]))
-        print("X10: " + str(solution[9]))
-    print("Result: " + str(func(solution)))
-    return solution
+    print final_runmin
+    print final_runmax
+    print final_runmean
+    print final_runstd
+    '''
+        print("X1: " + str(solution[0]))
+        print("X2: " + str(solution[1]))
+        if (dimensions == 5):
+            print("X3: " + str(solution[2]))
+            print("X4: " + str(solution[3]))
+            print("X5: " + str(solution[4]))
+        if (dimensions == 10):
+            print("X3: " + str(solution[2]))
+            print("X4: " + str(solution[3]))
+            print("X5: " + str(solution[4]))
+            print("X6: " + str(solution[5]))
+            print("X7: " + str(solution[6]))
+            print("X8: " + str(solution[7]))
+            print("X9: " + str(solution[8]))
+            print("X10: " + str(solution[9]))
+        print("Result: " + str(func(solution)))
+        return solution
+        '''
+    return plot_runs, final_runmin, final_runmax, final_runmean, final_runstd
+
 
 
 main()
