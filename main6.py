cc70ce03cfc99625fa372be103067e87d16543ad
Patrick <polandpatrick94@gmail.com>
2017-11-30 02:17:14 -0500

    Sudo multi processing

---
+++ main6.py
@@ -0,0 +1,44 @@
+import numpy as np
+from Functions import *
+from DE import optimize
+from PSO2 import opt
+from figgen import make_plot, make_table
+
+def main():
+    dims = [2,5,10]
+
+    funcs =[weirerstrass,]
+
+    tablesmax = []
+    tablesmin = []
+    tablesmean = []
+    tablesstd = []
+    tablesmaxp = []
+    tablesminp = []
+    tablesmeanp = []
+    tablesstdp = []
+    for func in funcs:
+        for dim in dims:
+            plot,tablemax,tablemin,tablemean,tablestd = optimize(dim,func)
+            plotp,tablemaxp,tableminp,tablemeanp,tablestdp = opt(dim,func)
+
+
+            make_plot(func,dim,plot,plotp)
+            make_plot(func,dim,plot)
+            #DO
+            tablesmax.append(tablemax)
+            tablesmin.append(tablemin)
+            tablesmean.append(tablemean)
+            tablesstd.append(tablestd)
+            #PSO
+            tablesmaxp.append(tablemaxp)
+            tablesminp.append(tableminp)
+            tablesmeanp.append(tablemeanp)
+            tablesstdp.append(tablestdp)
+            print('completed ', str(func.func_name), ' ', str(dim) )
+
+        make_table(func,tablesmax,tablesmin,tablesmean,tablesstd,tablesmaxp,tablesminp,tablesmeanp,tablesstdp)
+        make_table(func,tablesmaxp,tablesminp,tablesmeanp,tablesstdp)
+
+if __name__ == '__main__':
+    main()
