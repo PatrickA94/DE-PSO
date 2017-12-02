247b09d7cc0ee9dd34a718f4614bd0bb3eab3a5b
Patrick <polandpatrick94@gmail.com>
2017-12-02 13:25:12 -0500

    Speed improvment fixed 0.0 error for chart

--- DE.py
+++ DE.py
@@ -1,41 +1,67 @@
 import numpy as np
 from Functions import *
+import cProfile
+import random
 
 
 class population(object):
 
-    def __init__(self, dimensions, nump=100):
+    def __init__(self, dimensions,func ,nump=100 ):
 
         self.nump = nump
         self.dimensions = dimensions
         self.nump = nump
         self.rangep = {'max':10, 'min':-10}
         self.points = np.random.uniform(self.rangep['min'],self.rangep['max'],size=(self.dimensions,self.nump))
+        self.current_fitness = func(self.points)
 
     def setpoints(self,points):
         self.points = points
 
     def randselect(self):
-        X = np.random.choice(range(0,self.nump-1), size=3 , replace=False)
+        X =  random.sample(range(0,99),3)
         return X
 
     def mutation(self):
         F = 0.8
         v = self.points[:,self.randselect()[0]] + F * (self.points[:,self.randselect()[2]] - self.points[:,self.randselect()[1]])
+        if any(vin > 10 for vin in v):
+            for cnt,vin in enumerate(v):
+                if vin >10:
+                    v[cnt] = 10
+        if any(vin< -10 for vin in v):
+            for cnt,vin in enumerate(v):
+                if vin >10:
+                    v[cnt] = 10
         return v
 
     def crossover(self,mutatedvec,pointno):
         Cr = 0.9
         self.trialvec = np.empty([self.dimensions,1])
+        mutationscnt = 0
         for j in range(0,self.dimensions):
             if np.random.rand() < Cr:
                 self.trialvec[j] = mutatedvec[j]
+                mutationscnt+=1
             else:
                 self.trialvec[j] = self.points[j,pointno]
-        return self.trialvec
+        if mutationscnt== 0:
+            return self.trialvec,True
+        else:
+            return self.trialvec, False
+
 
     def get_points(self):
         return self.points
+
+    def get_node_fitness(self,i):
+        return self.current_fitness[i]
+
+    def get_current_fitness(self):
+        return self.current_fitness
+
+    def set_current_fitness(self,fitness,i):
+        self.current_fitness[i]=fitness
 
 
 
@@ -44,13 +70,13 @@ def optimize(dim,func):
     dimnesions = dim
     pop = 100
     max_nfc=300*dimnesions
-    runs = 3
+    runs = 2
     run=0
     allruns = []
     allrunsmax=[]
     allrunsmean=[]
     allrunsstd = []
-    nodes = population(dimnesions,pop)
+    nodes = population(dimnesions,func,pop)
     while (run < runs):
         rundata = []
         rundatamax=[]
@@ -59,18 +85,26 @@ def optimize(dim,func):
         nfc=0
         while( nfc < max_nfc):
             newnodes = np.empty([dimnesions,pop])
+
+
+
             for i in range(0,pop):
-                trialvec = nodes.crossover(nodes.mutation(),i)
-                if func(trialvec)<=func(nodes.get_points()[:,i]):
+                trialvec,functest = nodes.crossover(nodes.mutation(),i)
+                if functest == True:
+                    trialvec_fitness = nodes.get_node_fitness(i)
+                else:
+                    trialvec_fitness = func(trialvec)
+                if trialvec_fitness<=nodes.get_node_fitness(i):
+                    nodes.set_current_fitness(trialvec_fitness,i)
                     newnodes[:,i] = trialvec.reshape(dimnesions)
                 else:
                     newnodes[:,i] = nodes.get_points()[:,i]
             nodes.setpoints(newnodes)
             if nfc%pop == 0:
-                rundata.append(min(func(nodes.get_points())))
-                rundatamax.append(max(func(nodes.get_points())))
-                rundatamean.append(np.mean(func(nodes.get_points())))
-                rundatastd.append(np.std(func(nodes.get_points())))
+                rundata.append(min(nodes.get_current_fitness()))
+                rundatamax.append(max(nodes.get_current_fitness()))
+                rundatamean.append(np.mean(nodes.get_current_fitness()))
+                rundatastd.append(np.std(nodes.get_current_fitness()))
             nfc = nfc + 1
         run = run+1
         if run%10==0:
@@ -88,13 +122,12 @@ def optimize(dim,func):
     allrunsmax = np.reshape(allrunsmax,(runs,max_nfc/pop) )
     allrunsmean = np.reshape(allrunsmean,(runs,max_nfc/pop) )
     allrunsstd = np.reshape(allrunsstd,(runs,max_nfc/pop) )
-    final_runmin =allruns[:,-1]
-    final_runmax =allrunsmax[:,-1]
-    final_runmean =allrunsmean[:,-1]
-    final_runstd =allrunsstd[:,-1]
+    final_runmin =allruns
+    final_runmax =allrunsmax
+    final_runmean =allrunsmean
+    final_runstd =allrunsstd
     plot_runs = np.average(allruns,axis=0) # This is for the plots
 
     return plot_runs, final_runmin,final_runmax,final_runmean,final_runstd
 
-
-
+cProfile.run('optimize(2, highCond)',sort='tottime')
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from figgen import make_plot, make_table
 def main():
     dims = [2,5,10]
 
-    funcs =[highCond,]
+    funcs =[highCond,bentCig,discus,]
 
     tablesmax = []
     tablesmin = []
@@ -19,11 +19,11 @@ def main():
     tablesstdp = []
     for func in funcs:
         for dim in dims:
-            plot,tablemax,tablemin,tablemean,tablestd = optimize(dim,func)
-            plotp,tablemaxp,tableminp,tablemeanp,tablestdp = opt(dim,func)
+            plot,tablemin,tablemax,tablemean,tablestd = optimize(dim,func)
+            #plotp,tableminp,tablemaxp,tablemeanp,tablestdp = opt(dim,func)
 
 
-            make_plot(func,dim,plot,plotp)
+            #make_plot(func,dim,plot,plotp)
             make_plot(func,dim,plot)
             #DO
             tablesmax.append(tablemax)
@@ -31,14 +31,14 @@ def main():
             tablesmean.append(tablemean)
             tablesstd.append(tablestd)
             #PSO
-            tablesmaxp.append(tablemaxp)
-            tablesminp.append(tableminp)
-            tablesmeanp.append(tablemeanp)
-            tablesstdp.append(tablestdp)
+            tablesmaxp.append(tablemax)
+            tablesminp.append(tablemin)
+            tablesmeanp.append(tablemean)
+            tablesstdp.append(tablestd)
             print('completed ', str(func.func_name), ' ', str(dim) )
 
         make_table(func,tablesmax,tablesmin,tablesmean,tablesstd,tablesmaxp,tablesminp,tablesmeanp,tablesstdp)
-        make_table(func,tablesmaxp,tablesminp,tablesmeanp,tablesstdp)
+        #make_table(func,tablesmaxp,tablesminp,tablesmeanp,tablesstdp)
 
 if __name__ == '__main__':
     main()
